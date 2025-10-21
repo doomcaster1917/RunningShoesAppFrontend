db8a29b553f3f05f8bd977e9f37be2c3c2eeda13
Doomcaster1917 <Webtalestoday@gmail.com>
2025-10-21 16:05:17 +0300

    Added logs in middleware

--- middleware.js
+++ middleware.js
@@ -1,10 +1,18 @@
 import { NextResponse } from 'next/server';
 
 export function middleware(request) {
-    const initData = request.nextUrl.searchParams.get('tgWebAppData');
+    const initData = request.nextUrl.searchParams.get('initData'); // Проверяем initData
+    const url = request.nextUrl.href;
+    const queryParams = Object.fromEntries(request.nextUrl.searchParams);
+    const userAgent = request.headers.get('user-agent');
+
+    // Логи для отладки (видны в терминале)
+    console.log('Request URL:', url);
+    console.log('Query Parameters:', queryParams);
+    console.log('User-Agent:', userAgent);
 
     if (!initData) {
-        return new NextResponse('Forbidden', { status: 403 });
+        console.log('Access denied: No initData provided');
     }
 
     return NextResponse.next();
