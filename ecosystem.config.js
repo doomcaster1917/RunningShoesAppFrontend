f96c050866759b8a0fbdc50067e893dbf9fb6738
Doomcaster1917 <Webtalestoday@gmail.com>
2025-12-08 15:21:52 +0300

    Added ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    apps: [
+        {
+            name: 'my-next-app',
+            script: 'npm',
+            args: 'run start',
+            uid: 'shoesapp',
+            gid: 'shoesapp',
+            time: true
+        }
+    ]
+}
